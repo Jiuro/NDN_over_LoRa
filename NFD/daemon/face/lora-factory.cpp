5daafc622a7a35b92bf04db05c9fe5be0f7fd454
Camden <camdenewell@gmail.com>
2021-02-19 12:51:25 -0700

    Update lora-factory.cpp

--- NFD/daemon/face/lora-factory.cpp
+++ NFD/daemon/face/lora-factory.cpp
@@ -156,7 +156,7 @@ LoRaFactory::setup(){
 
   // Print a success message
   // Modified by C.EWELL
-  if (SX1272_debug_mode == SX1272Chip){
+  if (SX1272_board == SX1272Chip){
     NFD_LOG_INFO("SX1272 successfully configured");
     delay(1000);
   }
